efa3caca7c6ddaea10796d2d7eb7c34265bfd911
ganzob <gbatnasan5@gmail.com>
2024-03-07 09:54:54 +0400

    .

--- la/hw2_1.py
+++ la/hw2_1.py
@@ -1,9 +1,28 @@
 import numpy as np
 
-A = np.array([[0,2],[0,0],[0,0]])
+#A = np.array([[0,2],[0,0],[0,0]])
+A = np.array([[2,0,0],[0,2,1],[0,1,2],[0,0,0]])
 
-# Find the reduced SVD of A
-U, s, V = np.linalg.svd(A)
-print('U matrix:' ,U)
-print('singular values:',s)
-print('V matrix:',V)
+# (a) Reduced SVD of A
+U_r, S_r, Vt_r = np.linalg.svd(A,full_matrices=False)
+
+# (b) Full SVD of A
+U_f, S_f, Vt_f = np.linalg.svd(A, full_matrices=True)
+
+print(np.matmul(A.T,A))
+
+print("(a) Reduced SVD of A:")
+print("U_r:")
+print(U_r)
+print("S_r:")
+print(np.diag(S_r))
+print("Vt_r:")
+print(Vt_r)
+
+print("\n(b) Full SVD of A:")
+print("U_f:")
+print(U_f)
+print("S_f:")
+print(np.diag(S_f))
+print("Vt_f:")
+print(Vt_f)
--- la/svd.py
+++ la/svd.py
@@ -1,43 +1,70 @@
 import numpy as np
 
 A = np.array([[2,3],[0,2]])
+B = np.array([[-1, 1],[2,-1],[3,-5]])
 
-def svd(A):
-    # Compute the SVD of A
-    #U, s, V = np.linalg.svd(A)
-    U,s,V = None  ,None  ,None 
+def svd(matrix):
 
-    AAt = np.dot(A, A.T)
-    AtA = np.dot(A.T, A)
-    print(AAt)
-    print(AtA)
-
-    # Compute the eigenvalues and eigenvectors of AAt
-    evalues_AAt, evectors_AAt = np.linalg.eig(AAt)
-    print('eig values of AAt:',evalues_AAt)
-    print('eig vectors of AAt:',evectors_AAt)
-    # Compute the eigenvalues and eigenvectors of AtA
-
-    evalues_AtA, evectors_AtA = np.linalg.eig(AtA)
-
-    print('eig values of AtA:',evalues_AtA)
-    print('eig vectors of AtA:',evectors_AtA)
-
-    # Sort the eigenvalues and eigenvectors
-
-    # Compute the singular values
-    # Compute the singular vectors
-
+    AtA = np.dot(matrix.T, matrix)
+    AAt = np.dot(matrix, matrix.T)
     
-        
-    return U, s, V
+    eigenvalues_AtA, U = np.linalg.eigh(AtA)
+    eigenvalues_AAt, Vt = np.linalg.eigh(AAt)
+    
+    idx_AtA = np.argsort(eigenvalues_AtA)[::-1]
+    idx_AAt = np.argsort(eigenvalues_AAt)[::-1]
+    
+    eigenvalues_AtA = eigenvalues_AtA[idx_AtA]
+    eigenvalues_AAt = eigenvalues_AAt[idx_AAt]
+    
+    U = U[:, idx_AtA]
+    Vt = Vt[:, idx_AAt]
+    
+    sigma = np.sqrt(np.abs(eigenvalues_AtA))
+    rank = np.sum(eigenvalues_AtA > 0)
+    
+    U = U[:, :rank]
+    Vt = Vt[:, :rank]
+    
+    V = Vt.T
+    S = np.diag(sigma)
+    
+    return U, S, V
 
 
+print('--------SVD--------')
 U_comp, s_comp, V_comp = svd(A)
-U, s, V = np.linalg.svd(A)
-print('U matrix:' ,U) 
-print('hand implemented U matrix:', U_comp)
-print('singular values:',s)
-print('hand implemented s matrix:',s_comp)
-print('V matrix:',V)
-print('hand implemented V matrix:',V_comp)
+U, s, V = np.linalg.svd(A,full_matrices=False)
+print('U matrix:')
+print(U) 
+print('hand implemented U matrix:')
+print(U_comp)
+print('-------------------')
+print('singular values:')
+print(s)
+print('hand implemented s matrix:')
+print(s_comp)
+print('-------------------')
+print('V matrix:')
+print(V)
+print('hand implemented V matrix:')
+print(V_comp)
+
+
+print('--------SVD--------')
+U_comp, s_comp, V_comp = svd(B)
+U, s, V = np.linalg.svd(B,full_matrices=False)
+print('U matrix:')
+print(U) 
+print('hand implemented U matrix:')
+print(U_comp)
+print('-------------------')
+print('singular values:')
+print(s)
+print('hand implemented s matrix:')
+print(s_comp)
+print('-------------------')
+print('V matrix:')
+print(V)
+print('hand implemented V matrix:')
+print(V_comp)
