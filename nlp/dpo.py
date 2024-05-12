cdd02bafc01debc39bf8312c3ea78bdaea4d6cc4
ganzob <gbatnasan5@gmail.com>
2024-05-12 10:51:48 +0400

    .

---
+++ nlp/dpo.py
@@ -0,0 +1,6 @@
+from datasets import load_dataset
+from datasets import load_dataset_builder
+# output_dir = "/l/users/ganzorig.batnasan/data/"
+# builder = load_dataset_builder("SLPL/naab-raw")
+# builder.download_and_prepare(output_dir)
+dataset = load_dataset("lvwerra/stack-exchange-paired")
